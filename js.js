a13c4832d3fd013543004735fde33225b8d63328
Samiru73 <64921940+Samiru73@users.noreply.github.com>
2023-08-23 17:15:51 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.banner', { delay: 250 });
+ScrollReveal().reveal('.perfil', { delay: 400 });
+ScrollReveal().reveal('.descrip', { delay: 250 });
+ScrollReveal().reveal('.Projeitos', { delay: 350 });
+ScrollReveal().reveal('.card', { delay: 550 });
+ScrollReveal().reveal('.hab', { delay: 550 });
+ScrollReveal().reveal('.contact', { delay: 550 });
